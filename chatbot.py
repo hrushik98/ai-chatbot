e8f2a67f3e54812468ca2a0f6f8442283639e1d1
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2023-01-14 10:46:52 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,12 +6,12 @@ import os
 import openai
 
 
-openai.api_key = st.secrets['API_KEY']
+openai.api_key =  st.secrets['API_KEY']
 
 st.title("Created by @Hrush1k")
 if st.button("View Code"):
     st.write(f'''
-    <a target="_self" href="https://github.com/hrushik98">
+    <a target="_self" href="https://github.com/hrushik98/ai-chatbot">
         <button>
             github
         </button>
@@ -25,10 +25,12 @@ if st.button("to do list"):
     st.write("""
 To do list:
 1. Improve the User Interface  ✅ 
-2. Take care of punctuation
-3. Make it feel more chat-like
-4. Integrate human voice for input 
-5. Take care of token limit
+2. Fix issue with remembering chat history ✅ 
+2. Take care of punctuation ✅ 
+3. Make it feel more chat-like 
+4. Implement Internet search (via, google or bing) 
+5. Integrate human voice for input 
+6. Take care of token limit ✅ 
 """)
 st.text("")
 input_question = st.text_input("enter the message: ")
@@ -68,35 +70,56 @@ import csv
 
 
 def writetocsvh(question):
-    with open("results.csv", "w") as f:
+    with open("results.csv", "a") as f:
         data = csv.writer(f)
         name = "\nHuman:" + question
         data.writerow([name])
 def writetocsvb(question):
-    with open("results.csv", "w") as f:
+    with open("results.csv", "a") as f:
         data = csv.writer(f)
         name = "\nAI:" + question
         data.writerow([name])
-
+    
 if st.button("Go"):
-    writetocsvh(str(input_question))
-    with open("results.csv", 'r') as f:
-        data = csv.reader(f)
-        for i in data:
-            l1.append(i)
+    try:
+        writetocsvh(str(input_question))
+        with open("results.csv", 'r') as f:
+            data = csv.reader(f)
+            for i in data:
+                l1.append(i)
 
-    flat_list = []
+        flat_list = []
 
-    for sublist in l1:
-        for item in sublist:
-            flat_list.append(item)
+        for sublist in l1:
+            for item in sublist:
+                flat_list.append(item)
 
-    question = listToString(flat_list)
-    answer = ask(question)
+        question = listToString(flat_list)
+        answer = ask(question)
 
-    st.header(answer)
+        st.header(answer)
 
-    writetocsvb(answer)
+        writetocsvb(answer)
+    except:
+        os.remove("results.csv")
+        writetocsvh(str(input_question))
+        with open("results.csv", 'r') as f:
+            data = csv.reader(f)
+            for i in data:
+                l1.append(i)
+
+        flat_list = []
+
+        for sublist in l1:
+            for item in sublist:
+                flat_list.append(item)
+
+        question = listToString(flat_list)
+        answer = ask(question)
+
+        st.header(answer)
+
+        writetocsvb(answer)
 
 
 
